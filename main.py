7433c8f49637116fcc86541cef2ca0f7ee9a6f45
Sania2001 <saniagshinde@gmail.com>
2022-04-04 15:32:20 +0530

    .

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    return "Welcome to my website"
+    return "<h1>Welcome</h1>"
 
 @app.route("/contact")
 def Contact_Page():
-    return "Contact Page"
+    return render_template("contact.html")
 
 @app.route("/home")
 def HomePage():
